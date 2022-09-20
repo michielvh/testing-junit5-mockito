e42cc802cfd5963ccc05b40cacae7cccc1008bee
Michiel Van Huyck <mvanhuyck@cibg.brussels>
2022-09-20 13:59:05 +0200

    #8.2 : BDD given - when - then

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -14,7 +14,9 @@ import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.never;
@@ -53,18 +55,24 @@ class SpecialitySDJpaServiceTest {
 
     @Test
     void findByIdBddTest() {
+        // BDD : same test - written differently
 
+        //GIVEN
         Speciality speciality = new Speciality();
-
-        // BDD , written differently
         given(specialtyRepository.findById(1L)).willReturn(Optional.of(speciality));
 
+        //WHEN
         Speciality foundSpeciality = service.findById(1L);
 
+        //THEN
         assertThat(foundSpeciality).isNotNull();
 
-        verify(specialtyRepository,times(1)).findById(1L);
+       // verify(specialtyRepository,times(1)).findById(1L);
 
+        then(specialtyRepository).should().findById(anyLong());
+        //  ==  use one or the other
+        then(specialtyRepository).should(times(1)).findById(anyLong());
+        then(specialtyRepository).shouldHaveNoMoreInteractions();
     }
 
     @Test
